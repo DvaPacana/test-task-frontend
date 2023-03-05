250761e9868cf9d7abbe383ae96abcb63b828cda
otabek <36934609+Frankphoeinx@users.noreply.github.com>
2023-03-05 18:03:12 +0500

    created the project and finished the job with the specified conditions

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Block from "./components/Block.vue";
+import { left, right } from "../db";
+</script>
+
+<template>
+	<div class="main">
+		<Block
+			v-for="(block, key) in { left, right }"
+			:key="key"
+			:block="block"
+			:defaultLimit="key === 'left' ? 1 : 6"
+		/>
+	</div>
+</template>
+
+<style lang="scss">
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
+
+body {
+	background: #16161e;
+}
+
+.main {
+	height: 100vh;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+}
+</style>
