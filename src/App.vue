8ea9dae6ed8c9ff5dc0fc28fe5093ee723278502
Work <work@MacBook-Pro.local>
2023-07-18 19:30:50 +0300

    feat(testwork): init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+/// ///
+// Components
+const ItemsSideComponent = defineAsyncComponent(
+  () => import('@components/SideComponent.vue')
+)
+</script>
+
+<template>
+  <div id="test-app" class="test-app-container">
+    <ItemsSideComponent fetch-url="/user-items-list.json" multiple :max="6" />
+    <ItemsSideComponent fetch-url="/user-items-list.json" />
+  </div>
+</template>
+
+<style lang="scss">
+.test-app-container {
+  display: flex;
+}
+</style>
