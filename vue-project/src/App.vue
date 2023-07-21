7a5c46c4bb1dfb69b0a7187658662e4ce05946be
twogog <you@example.com>
2023-07-22 01:26:05 +0600

    done

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Bucket from "./components/Bucket.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Bucket />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  max-width: 1200px;
+  margin: 0 auto;
+  border: var(--border);
+  padding: 10px;
+}
+</style>
---
+++ vue-project/src/components/ShopCollection.vue
@@ -0,0 +1,52 @@
+<script setup>
+import items from "../assets/items/shopItems.json";
+import { ref } from "vue";
+
+const shopItems = ref(items);
+const addedItem = ref(null);
+
+function addItem({ target: { id } }) {
+  const currentItem = shopItems.value.find((i) => i.id === +id);
+  shopItems.value = shopItems.value.filter((i) => i.id !== +id);
+  addedItem?.value && shopItems.value.push(addedItem.value);
+  addedItem.value = currentItem;
+}
+
+function removeItem({ target: { id } }) {
+  shopItems.value.push(addedItem.value);
+  shopItems.value.push(addedItem.value);
+  addedItem.value = null;
+}
+</script>
+
+<template>
+  <div class="inner-container">
+    <div class="selected-container">
+      <p v-if="addedItem" @click="removeItem" :id="addedItem.id">
+        {{ addedItem.name }}
+      </p>
+      <p v-else>Select an item</p>
+    </div>
+
+    <ul class="not-selected">
+      <li @click="addItem" :id="item.id" v-for="item in shopItems">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.selected-container {
+  width: 50%;
+  margin-bottom: 10px;
+  margin-left: auto;
+  & p {
+    text-align: center;
+    min-height: 150px;
+    font-size: 30px;
+    cursor: pointer;
+    border: var(--border);
+  }
+}
+</style>
