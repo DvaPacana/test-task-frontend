e944583509abc86971483d6a13feee9c96f1011a
Dichell <9792910@gmail.com>
2023-08-09 21:21:05 +0300

    4 blocks with methods

---
+++ test-1/src/components/lists/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-sheet 
+        class="d-flex flex-wrap flex-row" 
+        rounded
+        :border=border
+        >
+            <v-btn 
+                v-for="itm in data" 
+                class="ma-2"
+                :key="itm.id" 
+                :text="itm.name" 
+                :color="itm.id === selected ? 'red' : ''"
+                @click="$emit('clicked', {...itm})"
+                >
+            </v-btn>
+    </v-sheet>
+</template>
+  
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import VueTypes from "vue-types";
+import { Item } from "@/utils/types";
+
+export default defineComponent({
+    name: "ListView",
+    props: {
+        data: {} as PropType<Item[]>,
+        selected: VueTypes.number.isRequired,
+        border: VueTypes.bool.def(true)
+    }
+
+});
+</script>
+  
---
+++ test-1/src/pages/Items.vue
@@ -0,0 +1,146 @@
+<template>
+    <section>
+
+        <v-row>
+            <v-col cols="6">
+                <v-sheet width="50%" border>
+                        <v-sheet min-height="10vh">
+                            <ListView :data="topLeft" :border="false"/>
+                        </v-sheet>
+                        <v-sheet class="ma-2">
+                            selected: {{topLeft.length}} / 6
+                        </v-sheet>
+                </v-sheet>
+            </v-col>
+            <v-col cols="6" class="d-flex justify-end">
+                <v-sheet width="50%" border>
+                    <h3>{{topRight.name}}</h3>
+                </v-sheet>
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col cols="6">
+                <ListView 
+                    :data="leftBlocks"
+                    @clicked="leftClick"
+                    />
+            </v-col>
+            <v-col cols="6">
+                <ListView 
+                    :data="rightBlocks" 
+                    @clicked="rightClick"
+                    />
+            </v-col>
+        </v-row>
+
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ListView from "@/components/lists/List.vue"
+import { Item } from "@/utils/types";
+
+export default defineComponent({
+    name: "ItemsView",
+    components: { ListView },
+    data() {
+
+        const leftBlocks = [
+            {
+                "id": 1,
+                "name": "Shoes 1"
+            },
+            {
+                "id": 2,
+                "name": "Shoes 2"
+            },
+            {
+                "id": 3,
+                "name": "Shoes 3"
+            },
+            {
+                "id": 4,
+                "name": "Shoes 4"
+            },
+            {
+                "id": 5,
+                "name": "T-shirt 1"
+            },
+            {
+                "id": 6,
+                "name": "T-shirt 2"
+            },
+            {
+                "id": 7,
+                "name": "T-shirt 3"
+            },
+            {
+                "id": 8,
+                "name": "T-shirt 4"
+            }
+        ]
+
+        const rightBlocks = [
+            {
+                "id": 11,
+                "name": "Jacket 1"
+            },
+            {
+                "id": 12,
+                "name": "Jacket 2"
+            },
+            {
+                "id": 13,
+                "name": "Jacket 3"
+            },
+            {
+                "id": 14,
+                "name": "Jacket 4"
+            },
+            {
+                "id": 15,
+                "name": "Hoodie 1"
+            },
+            {
+                "id": 16,
+                "name": "Hoodie 2"
+            },
+            {
+                "id": 17,
+                "name": "Hoodie 3"
+            },
+            {
+                "id": 18,
+                "name": "Hoodie 4"
+            }
+        ]
+
+        return { 
+            topLeft: [] as Item[], 
+            topRight: {} as Item, 
+            leftBlocks, 
+            rightBlocks 
+        };
+    },
+    methods:{
+        leftClick(val: Item) {
+            const findItem = this.topLeft.find((e)=> e.id == val.id)
+            if(!findItem){
+                if(this.topLeft.length <= 5){
+                    this.topLeft.unshift(val)
+                } else {
+                    this.topLeft.pop()
+                    this.topLeft.unshift(val)
+                }
+            }
+        },
+        rightClick(val: Item) {
+            this.topRight = val
+        }
+    }
+
+    });
+</script>
+  
